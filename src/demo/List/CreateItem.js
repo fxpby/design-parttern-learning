06044aa8ae4765d6ec7f7b739e5bd06e9f5bc0c3
fxpby <fxpby@outlook.com>
2022-02-08 00:54:32 +0800

    feat: add discount logic for item

--- src/demo/List/CreateItem.js
+++ src/demo/List/CreateItem.js
@@ -2,6 +2,23 @@ import Item from './Item.js'
 
 // 工厂函数
 export default function (list, itemData) {
-  console.log(list, itemData)
+  if (itemData.discount) {
+    itemData = createDiscount(itemData)
+  }
   return new Item(list, itemData)
+}
+
+// 代理模式, 感觉这里用有些生硬
+const createDiscount = itemData => {
+  return new Proxy(itemData, {
+    get: (target, key) => {
+      if (key === 'name') {
+        return `${target[key]}【折扣💰】`
+      }
+      if (key === 'price') {
+        return target[key] * 0.8
+      }
+      return target[key]
+    }
+  })
 }
