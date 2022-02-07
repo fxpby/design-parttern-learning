ea261214730a43d2857f3099ed1044a9ff7a5074
fxpby <fxpby@outlook.com>
2022-02-08 00:56:58 +0800

    feat: add class ShoppingCart logic

--- src/demo/ShoppingCart/ShoppingCart.js
+++ src/demo/ShoppingCart/ShoppingCart.js
@@ -1,9 +1,31 @@
+import getCart from '../ShoppingCart/GetCart.js'
+
 export default class ShoppingCart {
   constructor(app) {
     this.app = app
+    this.$el = document.createElement('div')
+    this.cart = getCart && getCart()
+  }
+
+  initBtn() {
+    const btnDom = document.createElement('button')
+    btnDom.innerHTML = '购物车'
+    btnDom.onclick = () => {
+      this.showCart()
+    }
+    this.$el.append(btnDom)
+  }
+
+  showCart() {
+    console.log(this.cart.getList())
+  }
+
+  render() {
+    this.app.$el.append(this.$el)
   }
 
   init() {
-
+    this.initBtn()
+    this.render()
   }
 }
